ff6dbbeeb096291257fe450374fd4a37e6345ccd
Anderson de Alencar <alencarbarros@protonmail.com>
2021-04-18 18:16:08 -0300

    PID pronto

--- MATLAB/PI.m
+++ MATLAB/PI.m
@@ -1,18 +1,11 @@
-% Calculo dos ângulos
-arctg = atand(Wd/alfa);
-theta1 = 180 - arctg;
-
-arctg = atand(Wd/(alfa - 22));   
-theta2 = 180 - arctg;
-
-arctg = atand(Wd/(54 - alfa));   
-theta3 = arctg;
-
-fi = -180 + (theta1 + theta2 + theta3);
+% Calculo dos ângulos 
+theta1 = 180 - atand(Wd/alfa);
+theta2 = 180 - atand(Wd/(alfa - 22));
+theta3 = atand(Wd/(54 - alfa));
 
 % Calculo de z do controlador PI
-tg = tand(fi);
-z = (-Wd/tg) - alfa;
+fi = -180 + (theta1 + theta2 + theta3);
+z = (-Wd/tand(fi)) - alfa;
 
 % Ganho Kc
 Kc = abs((s1*(s1 + 22)*(s1 + 54))/(98*(s1 - z)));
@@ -30,7 +23,7 @@ Gc_1 = (Kc * (s - z))/s;
 malha_aberta_1 = Gc_1 * Gp;
 malha_fechada_1 = feedback(malha_aberta_1, 1);
 
-step(malha_fechada_1);
+%step(malha_fechada_1);
 %rlocus(malha_fechada_1);
 %rlocus(malha_aberta_1);
 
@@ -42,7 +35,7 @@ Gc_2 = (Kc * (s + 24.2))/s;
 malha_aberta_2 = Gc_2 * Gp;
 malha_fechada_2 = feedback(malha_aberta_2, 1);
 
-step(malha_fechada_2);
+%step(malha_fechada_2);
 %rlocus(malha_fechada_2)
 %rlocus(malha_aberta_2)
 
